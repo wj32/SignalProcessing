ea0a09edae9652e8901a52e441b32234c3ac91fe
Wen Jia Liu <wj32.64@gmail.com>
2017-04-09 12:34:10 -0700

    Phase vocoder

--- SignalProcessing/Program.fs
+++ SignalProcessing/Program.fs
@@ -86,11 +86,32 @@ module Main =
       SampleBuffer.moveBy inputBuffer hopSize
     SampleBuffer.flush outputBuffer
 
+  let testExpandFrames (input : W.T) (parameters : P.T) (output : W.T) =
+    let frameSize = 2001
+    let hopSize = 500
+    let frames = (parameters.samples + hopSize - 1) / hopSize
+    let factor = 2
+    let inputBuffer = SampleBuffer.create (W.accessor input) AccessType.ReadOnly 50000
+    let outputBuffer = SampleBuffer.create (W.accessor output) AccessType.WriteOnly 50000
+    let frame = Array.zeroCreate frameSize
+    let wf = WindowFunction.hamming frameSize
+    let c = WindowFunction.normalizationFactor wf hopSize
+    for i = 0 to frames - 1 do
+      if i % factor = 0 then
+        SampleBuffer.window inputBuffer wf frame
+        SampleBuffer.moveBy inputBuffer hopSize
+      SampleBuffer.add outputBuffer c frame
+      SampleBuffer.moveBy outputBuffer hopSize
+    SampleBuffer.flush outputBuffer
+
+  let modTwoPi (x : float32) = float32 (Math.IEEERemainder(float x, 2. * Math.PI))
+
   let testAnalyzeFrames (input : W.T) (parameters : P.T) (output : W.T) =
     let frameSize = 8001
     let hopSize = 2000
     let fftSize = 16384 * 2
     let binSize = float32 parameters.sampleRate / float32 fftSize
+    let hopTime = float32 hopSize / float32 parameters.sampleRate
     let frames = (parameters.samples + hopSize - 1) / hopSize
     let fft = FFT.create fftSize FFT.Normalization.Symmetric
     let inputBuffer = SampleBuffer.create (W.accessor input) AccessType.ReadOnly 50000
@@ -98,12 +119,24 @@ module Main =
     let frame = Array.zeroCreate frameSize
     let fftInput = Array.zeroCreate fftSize
     let fftOutput = Array.zeroCreate fftSize
-    let wf = WindowFunction.blackman frameSize
-    let c = 0.5f * WindowFunction.normalizationFactor wf hopSize
+    let wf = WindowFunction.hann frameSize
+    let c = WindowFunction.normalizationFactor wf hopSize
+
+    // Phase vocoder
+    let prevPhase = Array.init 2 (fun _ -> Array.zeroCreate (fftSize / 2 + 1))
+    let sumPhase = Array.init 2 (fun _ -> Array.zeroCreate (fftSize / 2 + 1))
+    let binMag = Array.zeroCreate (fftSize / 2 + 1)
+    let binFreq = Array.zeroCreate (fftSize / 2 + 1)
+    let binMag' = Array.zeroCreate (fftSize / 2 + 1)
+    let binFreq' = Array.zeroCreate (fftSize / 2 + 1)
+    let twoPi = 2.f * float32 Math.PI
+    let phaseHop = twoPi * binSize * hopTime
+    let phaseDiffExpected =
+      Array.init (fftSize / 2 + 1) (fun i -> modTwoPi (float32 i * phaseHop))
 
     let passCutoffHz = 440.f
     let passCutoffBin = passCutoffHz / binSize
-    let rampWidthHz = 40.f
+    let rampWidthHz = 320.f
     let rampWidthBins = rampWidthHz / binSize
     let rampLeftBin = passCutoffBin - rampWidthBins / 2.f
     let rampRightBin = passCutoffBin + rampWidthBins / 2.f
@@ -119,7 +152,7 @@ module Main =
 
     let highPass i = 1.f - lowPass i
 
-    let filterToApply =
+    let mulFilter =
       let f = lowPass
       let a = Array.zeroCreate fftSize
       // Enforce symmetry
@@ -129,21 +162,64 @@ module Main =
         a.[i] <- a.[fftSize - i]
       a
 
-    for i = 0 to frames - 1 do
+    let mutable norm = 0.f
+
+    for frameIndex = 0 to frames - 1 do
       SampleBuffer.window inputBuffer wf frame
-      for channel = 1 to 2 do
+      for channel = 0 to 1 do
         FFT.sampleArrayToCentered channel frame fftInput
         FFT.compute fft fftInput fftOutput
 
-        for j = 0 to fftSize - 1 do
-          fftOutput.[j] <- Complex.mul' fftOutput.[j] filterToApply.[j]
+        //for j = 0 to fftSize - 1 do
+        //  fftOutput.[j] <- Complex.mul' fftOutput.[j] mulFilter.[j]
+
+        // Phase vocoder
+        for j = 0 to fftSize / 2 do
+          let phase = Complex.arg fftOutput.[j]
+          let phaseDiff = phase - prevPhase.[channel].[j]
+          prevPhase.[channel].[j] <- phase
+          let phaseDiffDev = phaseDiff - phaseDiffExpected.[j]
+          let phaseDiffDev = modTwoPi phaseDiffDev
+          let phaseDiffDevFrac = phaseDiffDev / phaseHop
+          binFreq.[j] <- (float32 j + phaseDiffDevFrac) * binSize
+          binMag.[j] <- Complex.abs fftOutput.[j]
+
+        // Shift
+        let halfTone = 2.f ** (1.f / 12.f)
+        let shiftFactor = halfTone ** (6.f)
+        Array.fill binMag' 0 (fftSize / 2 + 1) 0.f
+        Array.fill binFreq' 0 (fftSize / 2 + 1) 0.f
+        for j = 0 to fftSize / 2 do
+          let j' = int (float32 j * shiftFactor)
+          if j' <= fftSize / 2 then
+            binMag'.[j'] <- binMag'.[j'] + binMag.[j]
+            binFreq'.[j'] <- binFreq.[j] * shiftFactor
+        //for j = 0 to fftSize / 2 do
+        //  let j' = int (float32 j / shiftFactor)
+        //  if j' <= fftSize / 2 then
+        //    binMag'.[j] <- binMag.[j']
+        //    binFreq'.[j] <- binFreq.[j'] * shiftFactor
+
+        // Phase vocoder
+        for j = 0 to fftSize / 2 do
+          let freqDevFrac = binFreq'.[j] / binSize - float32 j
+          let phaseDiffDev = freqDevFrac * phaseHop
+          let phaseDiff = phaseDiffExpected.[j] + phaseDiffDev
+          let phase = sumPhase.[channel].[j] + phaseDiff
+          sumPhase.[channel].[j] <- phase
+          fftOutput.[j] <- Complex.ofPolar binMag'.[j] phase
+
+        for j = fftSize / 2 + 1 to fftSize - 1 do
+          fftOutput.[j] <- Complex.conj fftOutput.[fftSize - j]
 
         FFT.computeInverse fft fftOutput fftInput
         FFT.sampleArrayFromCentered channel frame fftInput
-      SampleBuffer.add outputBuffer c frame
+      SampleBuffer.add' outputBuffer c wf frame
+      norm <- max norm (SampleBuffer.norm outputBuffer frameSize)
       SampleBuffer.moveBy outputBuffer hopSize
       SampleBuffer.moveBy inputBuffer hopSize
     SampleBuffer.flush outputBuffer
+    printfn "Norm: %f" norm
 
   [<EntryPoint>]
   let main argv =
